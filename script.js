55b2f1b2a1326f3224cef03f9776540c2802fab6
POTHULA. JAI SAI <92910772+jaisaipothula@users.noreply.github.com>
2024-09-26 19:32:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let angle = 0;
+
+function rotateLine() {
+    angle += 2; // Increment the angle by 2 degrees
+    const line = document.getElementById('line');
+    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+}
+
+// Call rotateLine every 20 milliseconds
+setInterval(rotateLine, 20);
